2e490500d4a6531447ae470bc0720f525b37cf9f
Distrect <84394209+Distrect@users.noreply.github.com>
2023-10-14 18:09:03 +0300

    1

--- src/modules/authority/autharization/authorization.controller.ts
+++ src/modules/authority/autharization/authorization.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { AuthorityLoginBody } from 'src/modules/authority/autharization/authorization.dto';
 
 @Controller('authority')
 export class AuthorityAuthorizationController {
   constructor() {}
 
   @Post('/login')
-  public async Login(@Body() requestBody) {}
+  public async Login(@Body() requestBody: AuthorityLoginBody) {}
 }
---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import {
+  Injectable,
+  PipeTransform,
+  ArgumentMetadata,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class CustomValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    // const objectClass = plainToClass(metatype, value);
+    // const errors = await validate(objectClass);
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const formattedErrors = this.formatErrors(errors);
+      throw new HttpException('Message', HttpStatus.BAD_REQUEST);
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+
+  private formatErrors(errors: any[]) {
+    return errors.reduce((acc, err) => {
+      const { property } = err;
+      const constraints = Object.values(err.constraints);
+      acc[property] = constraints[0];
+      return acc;
+    }, {});
+  }
+}
