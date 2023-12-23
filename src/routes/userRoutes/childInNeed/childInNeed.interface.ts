eeac3f48b8fa39085da47dab765708b65fc999d1
Distrect <84394209+Distrect@users.noreply.github.com>
2023-12-23 22:47:56 +0200

    1
    1

--- src/routes/userRoutes/childInNeed/childInNeed.interface.ts
+++ src/routes/userRoutes/childInNeed/childInNeed.interface.ts
@@ -1,7 +1,10 @@
-import { IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { IFilterNeedGroup } from 'src/database/donation/needGroup/needGroup.DAO.interface';
 
 export class ListChildwithNeedsDTO {
   @IsOptional()
   filters: IFilterNeedGroup;
+
+  @IsNotEmpty()
+  page: number;
 }
